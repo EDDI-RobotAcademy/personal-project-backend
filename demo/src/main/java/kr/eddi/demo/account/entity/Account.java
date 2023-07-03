343f2bd62a1511cd21e8bee6cfbf2a5999f84f13
Jaekyoung Kim <88273159+jaegyoung@users.noreply.github.com>
2023-07-03 16:28:09 +0900

    feat: [김재경] 회원 등록 기능[WPB-1]

---
+++ demo/src/main/java/kr/eddi/demo/account/entity/Account.java
@@ -0,0 +1,27 @@
+package kr.eddi.demo.account.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String memberType;
+
+    public Account(String email, String password, String memberType) {
+        this.email = email;
+        this.password = password;
+        this.memberType= memberType;
+    }
+}
---
+++ demo/src/main/java/kr/eddi/demo/account/service/request/AccountRequest.java
@@ -0,0 +1,18 @@
+package kr.eddi.demo.account.service.request;
+
+import kr.eddi.demo.account.entity.Account;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class AccountRequest {
+    final private String email;
+    final private String password;
+    final private String memberType;
+
+    public Account toAccount () {
+        return new Account(email, password, memberType);
+    }
+
+}
