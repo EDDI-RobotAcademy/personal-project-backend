57f7f614d2e95d5bb40a884a069e098004e8aa24
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-07-12 11:03:32 +0900

    add: [남현기] Account Lazy Issue 해결 [OPB-10]

--- src/main/java/com/example/demo/domain/playlist/repository/PlaylistRepository.java
+++ src/main/java/com/example/demo/domain/playlist/repository/PlaylistRepository.java
@@ -2,6 +2,10 @@ package com.example.demo.domain.playlist.repository;
 
 import com.example.demo.domain.playlist.entity.Playlist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PlaylistRepository extends JpaRepository<Playlist, Long> {
+
+    @Query("SELECT DISTINCT p FROM Playlist p LEFT JOIN FETCH p.songList LEFT JOIN FETCH p.account WHERE p.id = :id")
+    Playlist findWithSongById(Long id);
 }
