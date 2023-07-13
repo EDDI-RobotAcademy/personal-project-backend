f784a50bd5fa476fb49db706371d1bbb3a08e4b4
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-07-13 19:28:39 +0900

    feat: [남현기] 특정 플레이리스트 내의 노래들 전달 [OPB-13]

---
+++ src/main/java/com/example/demo/domain/playlist/controller/form/PlaylistReadResponseForm.java
@@ -0,0 +1,15 @@
+package com.example.demo.domain.playlist.controller.form;
+
+import com.example.demo.domain.playlist.entity.Playlist;
+import com.example.demo.domain.song.entity.Song;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+public class PlaylistReadResponseForm {
+    final private Playlist playlist;
+    final private List<Song> songList;
+}
