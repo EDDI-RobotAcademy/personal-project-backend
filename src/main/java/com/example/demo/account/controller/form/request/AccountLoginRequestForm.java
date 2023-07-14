b09d9772acba18d4070d32ca95eb33735590f675
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-07-14 10:31:36 +0900

    Update AccountLoginRequestForm.java

--- src/main/java/com/example/demo/account/controller/form/request/AccountLoginRequestForm.java
+++ src/main/java/com/example/demo/account/controller/form/request/AccountLoginRequestForm.java
@@ -3,14 +3,12 @@ package com.example.demo.account.controller.form.request;
 
 import com.example.demo.account.entity.Account;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 public class AccountLoginRequestForm {
 
-    final private String email;
-    final private String password;
+    private String email;
+    private String password;
 
     public AccountLoginRequestForm(String email, String password) {
         this.email = email;
