98a3117e9f53b8989ed89bf2cc824098f612f34c
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-07-15 20:15:53 +0900

    add: [남현기] Spring Security 설정 추가 [OPB-9]

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -31,11 +32,12 @@ public class SecurityConfig {
         return httpSecurity
                 .httpBasic().disable()
                 .csrf().disable()
+                .cors().and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilterBefore(new JwtTokenFilter(accountService, secretKey, redisService, jwtTokenUtil), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
-                .requestMatchers("/account/register", "/account/login", "/account/email-check", "/account/nickname-check").permitAll()
+                .requestMatchers("/account/register", "/account/login", "/account/email-check", "/account/nickname-check", "/playlist/list").permitAll()
                 .anyRequest().hasAuthority(RoleType.NORMAL.name())
                 .and().build();
     }
