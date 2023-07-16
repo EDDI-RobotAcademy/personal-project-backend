858c72caa65d18f6c8e539ade15afdaaa4d783e3
Raun0129 <63294294+reBerel@users.noreply.github.com>
2023-07-17 06:29:33 +0900

    update: [장현민]로그인을 위한 controller생성[KWF-SIGN_IN-5]

--- src/main/java/com/example/demo/user/controller/UserController.java
+++ src/main/java/com/example/demo/user/controller/UserController.java
@@ -3,11 +3,15 @@ package com.example.demo.user.controller;
 import com.example.demo.user.controller.form.UserRegisterForm;
 import com.example.demo.user.controller.form.UserSignInRequestForm;
 import com.example.demo.user.controller.form.UserSignInResponseForm;
+import com.example.demo.user.controller.form.UserSignValidateForm;
+import com.example.demo.user.entity.User;
 import com.example.demo.user.service.UserService;
 import com.example.demo.user.service.request.UserSignUpRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -16,8 +20,24 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 public class UserController {
     final private UserService userService;
+
+    @GetMapping("/login/{uid}")
+    public User userLogin(@PathVariable("uid") String uid) {
+        log.info(uid);
+        return userService.logIn(uid);
+    }
+    @PostMapping("/sign-validate")
+    public List<String> userValidate(@RequestBody UserSignValidateForm userSignValidateForm) {
+        log.info(userSignValidateForm.getEmail());
+        log.info(userSignValidateForm.getNickName());
+        return userService.SignValidate(userSignValidateForm.toUserSignValidateRequest());
+    }
+
     @PostMapping("/sign-up")
     public Boolean userSignUp(@RequestBody UserRegisterForm registerForm) {
+        log.info(registerForm.getEmail());
+        log.info(registerForm.getName());
+        log.info(registerForm.getUid());
         return userService.signUp(registerForm.toUserSignUpRequest());
     }
     @PostMapping("sign-in")
