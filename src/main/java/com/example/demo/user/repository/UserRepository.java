570cd8693ba9ef883317121c3f29d0de356e0e4e
Raun0129 <63294294+reBerel@users.noreply.github.com>
2023-07-17 06:30:12 +0900

    update: [장현민]로그인을 위한 repository생성[KWF-SIGN_IN-7]

--- src/main/java/com/example/demo/user/repository/UserRepository.java
+++ src/main/java/com/example/demo/user/repository/UserRepository.java
@@ -7,4 +7,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
+    Optional<User> findByUid(String uid);
+    Optional<User> findByNickName(String nickName);
+
 }
