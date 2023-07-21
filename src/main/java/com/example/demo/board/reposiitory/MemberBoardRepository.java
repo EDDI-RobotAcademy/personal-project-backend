713ede588c8bb3de8efa31a39b45bb4a1d88c15b
YeoulCho <cyu5733@gmail.com>
2023-07-22 00:40:55 +0900

    fix: [조여울] 게시글 조회 쿼리문 추가 [CGS-5]

--- src/main/java/com/example/demo/board/reposiitory/MemberBoardRepository.java
+++ src/main/java/com/example/demo/board/reposiitory/MemberBoardRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.board.reposiitory;
 
 
 import com.example.demo.board.entity.MemberBoard;
+import io.lettuce.core.GeoArgs;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,7 +11,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberBoardRepository extends JpaRepository<MemberBoard, Long> {
-    List<MemberBoard> findAll();
-
+    @Query("SELECT b FROM MemberBoard b JOIN FETCH b.member")
+    List<MemberBoard> findAll(Sort sort);
+    @Query("SELECT b FROM MemberBoard b JOIN FETCH b.member WHERE b.boardId = :boardId")
+    Optional<MemberBoard> findByIdWithMember(Long boardId);
+    @Query("SELECT b FROM MemberBoard b JOIN FETCH b.member WHERE LOWER(b.content) LIKE %:keyword%")
     List<MemberBoard> findByContentContaining(String keyword);
 }
