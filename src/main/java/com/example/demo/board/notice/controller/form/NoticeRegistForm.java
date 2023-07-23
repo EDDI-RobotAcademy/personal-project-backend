8a1344bc36703e33b812be20ed80a4ba03bbe51e
qnfthslr <105690509+qnfthslr@users.noreply.github.com>
2023-07-23 23:04:27 +0900

    Update NoticeRegistForm.java

--- src/main/java/com/example/demo/board/notice/controller/form/NoticeRegistForm.java
+++ src/main/java/com/example/demo/board/notice/controller/form/NoticeRegistForm.java
@@ -1,23 +1,26 @@
 package com.example.demo.board.notice.controller.form;
 
 import com.example.demo.board.notice.entity.NoticeBoard;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.*;
 
 @Getter
-@ToString
-@RequiredArgsConstructor
-
+@NoArgsConstructor
 public class NoticeRegistForm {
-    final private String title;
-    final private String content;
+    private String title;
+    private String content;
 //    final private Long accountId;
 
 //    public NoticeBoard toNoticeBoard() {
 //        return new NoticeBoard(title, content,accountId);
 //    }
+
+
+    public NoticeRegistForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
     public NoticeBoard toNoticeBoard() {
         return new NoticeBoard(title, content);
     }
