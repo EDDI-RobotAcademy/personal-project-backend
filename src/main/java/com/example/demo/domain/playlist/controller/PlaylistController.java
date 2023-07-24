26e14949634da91350fcbfae21955b77f6fa7199
NamHyunKi <128335068+NamHyunKi@users.noreply.github.com>
2023-07-24 23:33:33 +0900

    .

--- src/main/java/com/example/demo/domain/playlist/controller/PlaylistController.java
+++ src/main/java/com/example/demo/domain/playlist/controller/PlaylistController.java
@@ -3,7 +3,6 @@ package com.example.demo.domain.playlist.controller;
 import com.example.demo.domain.playlist.controller.form.PlaylistModifyRequestForm;
 import com.example.demo.domain.playlist.controller.form.PlaylistReadResponseForm;
 import com.example.demo.domain.playlist.controller.form.PlaylistRegisterRequestForm;
-import com.example.demo.domain.playlist.entity.Playlist;
 import com.example.demo.domain.playlist.service.PlaylistService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
