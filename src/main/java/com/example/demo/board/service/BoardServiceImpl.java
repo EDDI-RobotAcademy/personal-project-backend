77b9b66513a0e15aad4a141464ff71c9b15c9c09
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-07-25 17:26:39 +0900

    update: [장현민] 게시판 페이지리스트 서비스 추가 [KWF-PAGELIST-2]

--- src/main/java/com/example/demo/board/service/BoardServiceImpl.java
+++ src/main/java/com/example/demo/board/service/BoardServiceImpl.java
@@ -2,12 +2,16 @@ package com.example.demo.board.service;
 
 import com.example.demo.board.controller.form.BoardCategoryListForm;
 import com.example.demo.board.controller.form.BoardCategoryResponseForm;
+import com.example.demo.board.controller.form.BoardResponseForm;
 import com.example.demo.board.controller.form.RequestBoardForm;
 import com.example.demo.board.entity.Board;
 import com.example.demo.board.entity.BoardCategory;
 import com.example.demo.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +25,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class BoardServiceImpl implements BoardService{
     final private BoardRepository boardRepository;
+
     @Override
     public List<Board> list() {
         return boardRepository.findAll(Sort.by(Sort.Direction.DESC, "boardId"));
@@ -87,6 +92,22 @@ public class BoardServiceImpl implements BoardService{
         }
         return boardCategoryList;
     }
+
+    @Override
+    public BoardResponseForm getBoardsByPage(int pageNumber, int pageSize) {
+        Pageable pageable = PageRequest.of(pageNumber, pageSize);
+        log.info("getBoardsByPage called with pageNumber: {}, pageSize: {}", pageNumber, pageSize);
+        Page<Board> boardPage = boardRepository.findAll(pageable);
+
+        // BoardResponseForm 객체 생성과 데이터 설정
+        BoardResponseForm boardResponse = new BoardResponseForm();
+        boardResponse.setData(boardPage.getContent());
+        boardResponse.setTotalPages(boardPage.getTotalPages());
+        boardResponse.setCurrentPage(boardPage.getNumber());
+
+        return boardResponse;
+    }
+
     @Override
     public List<BoardCategoryListForm> getCategoryList() {
         List<BoardCategoryListForm> categoryList = new ArrayList<>();
@@ -99,23 +120,4 @@ public class BoardServiceImpl implements BoardService{
         return categoryList;
     }
 
-//    @Override
-//    public Board findById(Long boardId) {
-//        Optional<Board> maybeBoard = boardRepository.findById(boardId);
-//        if (maybeBoard.isEmpty()) {
-//            log.info("정보가 없습니다.");
-//            return null;
-//        }
-//        return maybeBoard.get();
-//    }
-//
-//    @Override
-//    public Page<Board> boardList(Pageable pageable) {
-//        return boardRepository.findAll(pageable);
-//    }
-//
-//    @Override
-//    public Page<Board> boardSearchList(String searchKeyword, Pageable pageable) {
-//        return boardRepository.findByTitleContaining(searchKeyword, pageable);
-//    }
 }
