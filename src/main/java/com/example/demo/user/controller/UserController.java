a87baf0de42f2b314fc63d0136e7f9393bd8571b
hyunmin <63294294+reBerel@users.noreply.github.com>
2023-07-25 17:23:36 +0900

    feat: [장현민] 북마크 기능 컨트롤러 생성 [KWB-BOOKMARK-1]

--- src/main/java/com/example/demo/user/controller/UserController.java
+++ src/main/java/com/example/demo/user/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.example.demo.user.controller;
 
+import com.example.demo.board.dto.BoardDto;
 import com.example.demo.user.controller.form.UserRegisterForm;
 import com.example.demo.user.controller.form.UserSignInRequestForm;
 import com.example.demo.user.controller.form.UserSignInResponseForm;
 import com.example.demo.user.controller.form.UserSignValidateForm;
+import com.example.demo.user.dto.UserDto;
+import com.example.demo.user.entity.Bookmark;
 import com.example.demo.user.entity.User;
 import com.example.demo.user.service.UserService;
 import com.example.demo.user.service.request.UserSignUpRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -40,11 +45,23 @@ public class UserController {
         log.info(registerForm.getUid());
         return userService.signUp(registerForm.toUserSignUpRequest());
     }
-    @PostMapping("sign-in")
+    @PostMapping("/sign-in")
     public UserSignInResponseForm UserSignIn(@RequestBody UserSignInRequestForm signInRequestForm){
         return userService.signIn(signInRequestForm.toUserSignInRequest());
     }
+    @DeleteMapping("/{userId}")
+    public void signDown(@PathVariable("userId") Long userId) {
+        log.info("signDown()");
+        userService.delete(userId);
+    }
+    @PostMapping("/bookmark/{userId}")
+    public ResponseEntity<String> addBookmark(@PathVariable Long userId, @RequestBody Long boardId) {
+        log.info("ResponseEntity()");
+        return ResponseEntity.ok("북마크 추가 성공");
+    }
 
+    @DeleteMapping("/bookmark/{userId}")
+    public ResponseEntity<String> deleteBookmark(@PathVariable Long userId, @RequestBody Long boardId) {
+        return ResponseEntity.ok("북마크 삭제 성공");
+    }
 }
-
-
