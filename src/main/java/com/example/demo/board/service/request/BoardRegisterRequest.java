728c2f73e0aca124377d4dab59c405223c4f9a10
Raun0129 <63294294+reBerel@users.noreply.github.com>
2023-07-29 06:12:41 +0900

    update: [장현민]ckeditor으로 인한 request 변경 [KWB-BOARD-3]

--- src/main/java/com/example/demo/board/service/request/BoardRegisterRequest.java
+++ src/main/java/com/example/demo/board/service/request/BoardRegisterRequest.java
@@ -6,6 +6,13 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 @Getter
 @RequiredArgsConstructor
 public class BoardRegisterRequest {
@@ -15,6 +22,38 @@ public class BoardRegisterRequest {
     final private BoardCategory category;
 
     public Board toBoard() {
-        return new Board(writer, title, content, category);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
+        Calendar time = Calendar.getInstance();
+        String fileName = writer
+                + sdf.format(time.getTime())
+                + ".txt";
+        try {
+            String path = System.getProperty("user.dir")
+                    + File.separator	// Windows('\'), Linux, MAC('/')
+                    + "board";
+            System.out.println("절대 경로 : " + path);
+            File folder = new File(path);
+            if (!folder.exists()) {	// 폴더가 존재하는지 체크, 없다면 생성
+                if (folder.mkdir())
+                    System.out.println("폴더 생성 성공");
+                else
+                    System.out.println("폴더 생성 실패");
+            } else {	// 폴더가 존재한다면
+                System.out.println("폴더가 이미 존재합니다.");
+            }
+            File file = new File(path
+                    + File.separator
+                    + fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter writer = new BufferedWriter(fw);
+            writer.write(content);
+            writer.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return new Board(writer, title, fileName, category);
     }
 }
